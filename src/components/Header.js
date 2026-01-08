da2e74cdc744edb4c32b88df56c7fe3e5de1f562
alisoonnn <145472630+alisoonnn@users.noreply.github.com>
2026-01-08 03:31:02 +0100

    mobile issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/images/andi.svg";
 import { useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const Header = () => {
     const location = useLocation();
-    const isProjectsPage = location.pathname === '/projects';
     const [scrollPos, setScrollPos] = useState(0);
     const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 768px)').matches);
+    
     const isProjectDetailPage = location.pathname.startsWith('/projects/') && location.pathname !== '/projects';
     const isOnAnyProjectPage = location.pathname.startsWith('/projects');
 
@@ -31,28 +31,27 @@ const Header = () => {
         mobileQuery.addEventListener('change', checkMobile);
         return () => mobileQuery.removeEventListener('change', checkMobile);
     }, []);
-
-    const isProjectsVisible = scrollPos > 100;
     
     return (
-        <motion.header 
-            className='header'
-        >
-           <motion.a 
-            href="/" 
-            className="logo-link"
-            animate={{
-                scale: !isMobile && isOnAnyProjectPage ? 0.5 : 1,
-                y: !isMobile && isOnAnyProjectPage ? -280 : 0,
-                opacity: isMobile && isOnAnyProjectPage ? 0.2 : 1,  // ← Ajout opacity
-            }}
-            transition={{ 
-                duration: isProjectDetailPage ? 0 : 0.4,
-                ease: "easeInOut" 
-            }}
-        >
-            <img src={logo} alt="Andi Logo" />
-        </motion.a>
+        <motion.header className='header'>
+            <motion.a 
+                href="/" 
+                className="logo-link"
+                animate={!isMobile ? {
+                    // Animation desktop
+                    scale: isOnAnyProjectPage ? 0.5 : 1,
+                    y: isOnAnyProjectPage ? -280 : 0,
+                } : {
+                    // Animation mobile (juste opacity)
+                    opacity: isOnAnyProjectPage ? 0.2 : 1
+                }}
+                transition={{ 
+                    duration: isProjectDetailPage ? 0 : 0.4,
+                    ease: "easeInOut" 
+                }}
+            >
+                <img src={logo} alt="Andi Logo" />
+            </motion.a>
         </motion.header>
     );
 }
