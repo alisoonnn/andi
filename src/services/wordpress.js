de1df088dc5b68900f9e6f8563a6859202c691cd
alisoonnn <145472630+alisoonnn@users.noreply.github.com>
2026-01-08 04:43:52 +0100

    mobile issue

--- src/services/wordpress.js
+++ src/services/wordpress.js
@@ -1,5 +1,5 @@
 // const WORDPRESS_GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL || 'https://andi-design.fr/graphql';
-const WORDPRESS_GRAPHQL_URL = 'https://admin.andidet.cluster100.hosting.ovh.net/graphql';
+const WORDPRESS_GRAPHQL_URL = 'https://admin.andi-design.fr/graphql';
 
 export const fetchProjects = async () => {
   try {
