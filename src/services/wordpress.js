0b476c3ba1f90dc94588925edfbd7575617ee1e6
alisoonnn <145472630+alisoonnn@users.noreply.github.com>
2026-01-10 13:08:40 +0100

    adding images

--- src/services/wordpress.js
+++ src/services/wordpress.js
@@ -27,71 +27,41 @@ export const fetchProjects = async () => {
         }
       }
       projet {
-        imageDuProjet1 {
-          node {
-            sourceUrl
-            altText
-            mediaDetails {
-              width
-              height
-            }
-          }
-        }
-        imageDuProjet2 {
-          node {
-            sourceUrl
-            altText
-            mediaDetails {
-              width
-              height
-            }
-          }
-        }
-        imageDuProjet3 {
-          node {
-            sourceUrl
-            altText
-            mediaDetails {
-              width
-              height
-            }
-          }
-        }
-        imageDuProjet4 {
-          node {
-            sourceUrl
-            altText
-            mediaDetails {
-              width
-              height
-            }
-          }
-        }
-        imageDuProjet5 {
-          node {
-            sourceUrl
-            altText
-            mediaDetails {
-              width
-              height
-            }
-          }
-        }
-        imageDuProjet6 {
-          node {
-            sourceUrl
-            altText
-            mediaDetails {
-              width
-              height
-            }
-          }
-        }
+        imageDuProjet1 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet2 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet3 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet4 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet5 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet6 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet7 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet8 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet9 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet10 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet11 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet12 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet13 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet14 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet15 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet16 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet17 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet18 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet19 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet20 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet21 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet22 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet23 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet24 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet25 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet26 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet27 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet28 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet29 { node { sourceUrl altText mediaDetails { width height } } }
+        imageDuProjet30 { node { sourceUrl altText mediaDetails { width height } } }
         videoDuProjet {
-                    node {
-    mediaItemUrl
-  }
-                  }
+          node {
+            mediaItemUrl
+          }
+        }
         category
       }
     }
